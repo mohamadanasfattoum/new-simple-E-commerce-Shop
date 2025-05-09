fe46ea833c9c6b2c8d38d925eebcd4db46ca8521
mohamadanasfattoum <samaanas30@gmail.com>
2025-05-09 11:45:35 +0200

    updated description field to get more space

--- products/models.py
+++ products/models.py
@@ -6,7 +6,7 @@ from django.db import models
 class Product(models.Model):
     name = models.CharField(max_length=255)
     short_description = models.TextField(max_length=300)
-    description = models.TextField(max_length=1000)
+    description = models.TextField(max_length=10000)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField() # Quantity in stock
 
