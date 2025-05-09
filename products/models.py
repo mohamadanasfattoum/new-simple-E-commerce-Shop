a2fc823bd1c17a551da35988f2064a2a62a390cb
mohamadanasfattoum <samaanas30@gmail.com>
2025-05-09 09:39:21 +0200

    created a Product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    short_description = models.TextField(max_length=300)
+    description = models.TextField(max_length=1000)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField() # Quantity in stock
+
+
+    def __str__(self):
+        return self.name
